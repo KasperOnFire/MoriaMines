4eb189a7000422777d892264a9cb58c97ba85b68
Kasper Ravn Breindal <breindal70@gmail.com>
2016-10-25 21:36:28 +0200

    added rusty pickaxe

--- src/moriamines/MoriaMines.java
+++ src/moriamines/MoriaMines.java
@@ -6,7 +6,7 @@ public class MoriaMines {
 
     public static void main(String[] args) throws IOException {
         GameControl gc = new GameControl();
-        
+
         System.out.println("Welcome to the Mines of Moria!");
         gc.gameSetup();
         gc.gameRun();
